fb119bbf820aa81ba93727f7573cb26ed9f755c9
Bruno Guilherme <dasilvaguilhermebruno@gmail.com>
2024-03-19 19:27:41 -0300

    feat: layout completed

--- unicap-SIn/residencia-RiseUp/learn/my_app/lib/main.dart
+++ unicap-SIn/residencia-RiseUp/learn/my_app/lib/main.dart
@@ -1,13 +1,17 @@
 // Importa o pacote material 3
 import 'package:flutter/material.dart';
+import 'package:my_app/pages/buttonSection/button-section.dart';
+import 'package:my_app/pages/imageSection/image-section.dart';
+import 'package:my_app/pages/textSection/text-section.dart';
+import 'package:my_app/pages/titleSection/title-section.dart';
 
 // Inicializa a raiz do projeto
 void main() => runApp(const MyApp());
 
-
-// Configuração geral da página
+// Configuração geral da aplicação
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
@@ -22,26 +26,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(appTitle),
         ),
-        body: const Center(
-          child: Text('Hello bruno'),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageSection(
+                image: 'images/lake.jpg',
+              ),
+              TitleSection(
+                name: 'Oeschinen Lake Campground',
+                location: 'Kandersteg, Switzerland',
+              ),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
+                    'is one of the larger Alpine Lakes. A gondola ride from '
+                    'Kandersteg, followed by a half-hour walk through pastures '
+                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'degrees Celsius in the summer. Activities enjoyed here '
+                    'include rowing, and riding the summer toboggan run.',
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class TitleSection extends StatelessWidget {
-  const TitleSection({
-    super.key,
-    required this.name,
-    required this.location,
-  });
-
-  final String name;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Placeholder();
-  }
-}
